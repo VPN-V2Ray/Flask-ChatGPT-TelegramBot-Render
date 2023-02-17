3ade1d3bc3fa22df5ed83fb6196b8274c91f9d9e
VPN-V2Ray <46552880+VPN-V2Ray@users.noreply.github.com>
2023-02-17 23:01:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ LANGUAGE_TABLE = {
 class Prompts:
 	    def __init__(self):
 	        self.msg_list = []
-	        self.msg_list.append(f"ChatGPT:{LANGUAGE_TABLE[chat_language]}")
+	        self.msg_list.append(f"ChatGPT😊:{LANGUAGE_TABLE[chat_language]}")
 	    
 	    def add_msg(self, new_msg):
 	        if len(self.msg_list) >= MSG_LIST_LIMIT:
